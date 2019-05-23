4d528b2d595b7504926fae4927784b8a8feaa82f
Pin Lin <moneycat711@gmail.com>
2019-05-23 10:40:16 +0800

    Send two messages back

--- bot.py
+++ bot.py
@@ -35,9 +35,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    messages = []
+
+    messages += [TextSendMessage(text=event.message.text)]
+    messages += [TextSendMessage(text=event.message.text)]
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        messages)
 
 
 if __name__ == '__main__':
