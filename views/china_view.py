5e54ac8a57a0251ff36fec83ea64d43e983bf5dc
Pin Lin <moneycat711@gmail.com>
2019-05-23 14:43:34 +0800

    Change sticker of ChinaView

--- views/china_view.py
+++ views/china_view.py
@@ -21,5 +21,5 @@ class ChinaView(View):
                 text="當時我們決定以通訊安全為大方向，研究了數個市面上較知名的通訊平台（Line、WhatsApp、Telegram⋯⋯等）在防止通訊內容外洩的相關作為，發現 E2EE 在其中扮演了很重要的角色。"),
             TextSendMessage(
                 text="於是我們做出一個簡單的通訊平台，在實作過程中我們對於 E2EE 訊息傳輸的整個流程更了解了。"),
-            StickerSendMessage(package_id=3, sticker_id=213),
+            StickerSendMessage(package_id=3, sticker_id=216),
         ]
