25a93f640ab208dff6956c0d02cdf67e059442f5
Pin Lin <moneycat711@gmail.com>
2019-05-23 20:59:28 +0800

    Hide name of parameters

--- views/url_view.py
+++ views/url_view.py
@@ -16,10 +16,10 @@ class UrlView(View):
         """
         responses += [
             TextSendMessage(
-                text="這個專案是我興趣使然的，會想寫是因為一直在使用的 goo.gl 縮網址服務不再提供了..."),
+                "這個專案是我興趣使然的，會想寫是因為一直在使用的 goo.gl 縮網址服務不再提供了..."),
             TextSendMessage(
-                text="然後學校的網路又會對知名的 bit.ly 縮網址服務中間人攻擊（截至 2019-05-23 為止仍是）"),
+                "然後學校的網路又會對知名的 bit.ly 縮網址服務中間人攻擊（截至 2019-05-23 為止仍是）"),
             TextSendMessage(
-                text="剛好趁這個機會來自己實作一個，也覺得如果有自己的縮網址服務很酷ｘＤ"),
+                "剛好趁這個機會來自己實作一個，也覺得如果有自己的縮網址服務很酷ｘＤ"),
             StickerSendMessage(package_id=3, sticker_id=209),
         ]
