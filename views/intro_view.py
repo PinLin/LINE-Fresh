959b50f79caed797fa97643bb17aa7317e9d783d
Pin Lin <moneycat711@gmail.com>
2019-05-25 02:16:12 +0800

    Edit text of IntroView

--- views/intro_view.py
+++ views/intro_view.py
@@ -22,7 +22,7 @@ class IntroView(View):
             TextSendMessage(
                 "平常喜歡做些小專案，去解決生活上的一些問題。因為遇到痛處，克服它的同時也能夠磨練到自己的技術。"),
             TextSendMessage(
-                "我是個樂於分享的人，平常和同學、朋友交流學習的心得，展示自己小專案的結果。"),
+                "我是個樂於分享的人，平常和同學、朋友交流學習的心得，或是展示自己小專案的結果。"),
             TextSendMessage(
                 "在其他人遇到困難的時候，我都會二話不說地幫忙看看，因為我喜歡幫助人的感覺，而且在過程中，常常能學到許多原本不會的東西。"),
             TemplateSendMessage("還想知道更多？", template=self.__intro_template()),
