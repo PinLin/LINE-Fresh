f564bdc62bb6c4847ef74c38b2df266c49254bd8
Pin Lin <moneycat711@gmail.com>
2019-06-11 22:23:06 +0800

    Update mechanism of view triggering

--- views/view.py
+++ views/view.py
@@ -9,16 +9,23 @@ class View:
         """
         return ['測試', 'test']
 
-    def trigger(self, responses: list, text: str) -> None:
+    def _judge(self, keywords: list, text: str) -> bool:
         """
-        觸發 view 的機制
+        設定觸發的條件
         """
         # 如果訊息文字中存在任一關鍵字
         for keyword in self._keywords:
             if keyword.lower() in text.lower():
-                # 編輯回應的訊息列表
-                self.main(responses, text)
-                return
+                return True
+
+        return False
+
+    def trigger(self, responses: list, text: str) -> None:
+        """
+        觸發 view 的機制
+        """
+        if self._judge(self._keywords, text):
+            self.main(responses, text)
 
     def main(self, responses: list, text: str) -> None:
         """
